a0c080383e4d48395adc5544cbccf7301c34c821
Edward Huang <26125482@qq.com>
2018-05-31 23:05:04 +0800

    fix the bug

--- TestOne/Tryone.py
+++ TestOne/Tryone.py
@@ -4,7 +4,7 @@
 import webbrowser
 c = webbrowser.open('http://weibo.com')
 
-通过一个dict来录入股票和股票代码的关系并打印出来
+# 通过一个dict来录入股票和股票代码的关系并打印出来
 d = {'工商银行': '601398', '中国银行': '601988', '平安银行': '000001'}
 stockname = input('请输入股票的名称：')
 shocknumber = input('请输入股票的代码:')
@@ -12,4 +12,4 @@ d[stockname]=shocknumber
 print(d)
 print(d['工商银行'])
 print(d[stockname])
-变量在[]中可以不加入引号了。这个比较好。
+# 变量在[]中可以不加入引号了。这个比较好。
